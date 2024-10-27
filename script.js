edc2da05919b00be63ee84041d972fed8ec56f1b
mhilham <mhilham16@gmail.com>
2024-10-27 21:16:17 +0700

    menambahkan fungsi untuk menghilngkan navbar ngeklik di luar elemen

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const navbarNav = document.querySelector('.navbar-nav');
 document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle('active');
 };
+
+//klik di luar elemen
+const hm = document.querySelector('#hamburger-menu');
+
+
+document.addEventListener('click', function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+  });
